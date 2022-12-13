6fdbc0c28d3d98ad0574507c3794ea1266c9849a
tianzhengdong <suming@sumingdeMacBook-Air.local>
2022-12-13 14:54:38 +0800

    管理资源

---
+++ learn_webpack/src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpackconfig'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
+  return element;
+}
+
+document.body.appendChild(component());
