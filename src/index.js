e570364559c68c716ee8ce5d5f529ee7f4679408
tianzhengdong <suming@sumingdeMacBook-Air.local>
2022-12-13 17:11:32 +0800

    webpack-dev-middleware

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpackconfigaaa'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpackconfig'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
   element.onclick = printMe;
