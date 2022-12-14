1baffeb3ca8a5fac06dce717dff225f7dd9b3f1d
tianzhengdong <suming@sumingdeMacBook-Air.local>
2022-12-14 19:23:38 +0800

    环境变量没设置成功

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,16 @@
-
 const path = require('path');
-// 动态生成index.html 动态引入资源
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  // 编译入口
-  entry: {
-    index: './src/index.js',
-  },
-  // 插件
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: '创建library',
-    }),
-  ],
-  // 编译出口
-  output: {
-    filename: 'webpack-numbers.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      name: 'webpackNumbers',
-      type: 'umd',
+module.exports = (env) => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log('env: ', env); // 'local'
+  console.log('Goal: ', env.goal); // 'local'
+  console.log('Production: ', env.production); // true
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
-    clean: true,
-  },
-  externals: {
-    lodash: {
-      commonjs: 'lodash',
-      commonjs2: 'lodash',
-      amd: 'lodash',
-      root: '_',
-    },
-  },
+  };
 };
