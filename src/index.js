db4dd00446b65fa6cd3887f601ccaa39a2422c3c
tianzhengdong <suming@sumingdeMacBook-Air.local>
2022-12-14 09:16:15 +0800

    缓存（有疑问）

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// import Print from './print';
+
 async function getComponent() {
   const element = document.createElement('div');
   const { default: _ } = await import('lodash');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.onclick = Print.bind(null, 'Hello webpack!');
   return element;
  }
  getComponent().then((component) => {
