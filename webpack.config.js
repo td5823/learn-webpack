2dcc20a3c196a5f32a62f242475d33319cd73bb3
tianzhengdong <suming@sumingdeMacBook-Air.local>
2022-12-15 19:46:15 +0800

    热模块替换

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
-module.exports = (env) => {
-  // Use env.<YOUR VARIABLE> here:
-  console.log('env: ', env); // 'local'
-  console.log('Goal: ', env.goal); // 'local'
-  console.log('Production: ', env.production); // true
-
-  return {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-  };
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    // Runtime code for hot module replacement
+    hot: 'webpack/hot/dev-server.js',
+    // Dev server client for web socket transport, hot and live reload logic
+    client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    // Dev server client for web socket transport, hot and live reload logic
+    hot: false,
+    client: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
+    }),
+    // Plugin for hot module replacement
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
 };
